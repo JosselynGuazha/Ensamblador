e1ec806ceae66e7ee3b7ff380048ccfdca4a1ab2
JosselynGuazh <josseguazha@gmail.com>
2020-03-07 01:29:12 -0500

    pilas

---
+++ pilas/pila.asm
@@ -0,0 +1,37 @@
+%macro escribir 2
+	mov eax,4
+	mov ebx,1
+	mov ecx,%1
+	mov edx,%2
+	int 80h
+
+%endmacro
+
+section .data
+    msj db "*"
+
+section .text 
+    global _start:
+_start:
+
+
+    mov ecx,9
+    jmp principal
+
+principal:
+    cmp ecx,0
+    jz salir
+    jmp imprimir
+
+imprimir:
+    dec ecx
+    push ecx
+    escribir msj,1
+    pop ecx 
+    jmp principal
+
+
+salir:
+    mov eax,1
+    int 80H
+
